94d5ad7d32378825affb37b2c598e76d23fd4522
abowspy <abowspy@gmail.com>
2020-10-04 14:45:52 +0800

    init commit

---
+++ app/src/main/java/tw/abowspy/googleroomwordssample/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.abowspy.googleroomwordssample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
